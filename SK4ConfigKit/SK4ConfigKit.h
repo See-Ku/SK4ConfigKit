c685d139eb9eb9016c1a781a68386bb58260cae0
See.Ku <info@see-ku.com>
2016-04-18 07:56:15 +0900

    Initial Commit

---
+++ SK4ConfigKit/SK4ConfigKit.h
@@ -0,0 +1,19 @@
+//
+//  SK4ConfigKit.h
+//  SK4ConfigKit
+//
+//  Created by See.Ku on 2016/04/18.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SK4ConfigKit.
+FOUNDATION_EXPORT double SK4ConfigKitVersionNumber;
+
+//! Project version string for SK4ConfigKit.
+FOUNDATION_EXPORT const unsigned char SK4ConfigKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SK4ConfigKit/PublicHeader.h>
+
+
---
+++ SK4ConfigKitTests/SK4ConfigKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  SK4ConfigKitTests.swift
+//  SK4ConfigKitTests
+//
+//  Created by See.Ku on 2016/04/18.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+import XCTest
+@testable import SK4ConfigKit
+
+class SK4ConfigKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
